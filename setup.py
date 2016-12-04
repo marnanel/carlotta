6c03bb1fd5c3746fad6ca42a60248a34e9345407
Thomas Thurman <thomas@thurman.org.uk>
2016-12-04 02:09:25 +0000

    Started to introduce setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Entropy of Carlotta',
+		version='0.1',
+		description='Mailing list system',
+		author='Marnanel Thurman, Lars Wirzenius',
+		author_email='marnanel@thurman.org.uk',
+		url='https://github.com/tthurman/carlotta',
+		packages=['carlotta'],
+		scripts=['scripts/carlotta'],
+     )
