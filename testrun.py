79b60a6dfa6eaf72720c36232905804ce538a1f7
Thomas Thurman <thomas@thurman.org.uk>
2016-12-05 14:22:41 +0000

    Tests compatible with nose

--- testrun.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import os
-
-suite = unittest.TestSuite()
-for n in filter(lambda fn: fn[-8:] == "Tests.py", os.listdir(".")):
-    suite.addTest(unittest.defaultTestLoader.loadTestsFromName(n[:-3]))
-
-runner = unittest.TextTestRunner()
-runner.run(suite)
